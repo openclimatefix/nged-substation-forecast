486a15360ac217b7bf6986c288ebed93ebd839c0
Jack Kelly <jack@openclimatefix.org>
2026-02-03 18:20:44 +0000

    Conversion to Parquet works!

--- src/nged_substation_forecast/defs/nged_assets.py
+++ src/nged_substation_forecast/defs/nged_assets.py
@@ -3,6 +3,7 @@
 from datetime import timedelta
 from pathlib import Path, PurePosixPath
 
+import polars as pl
 from dagster import (
     AddDynamicPartitionsRequest,
     AssetExecutionContext,
@@ -21,6 +22,7 @@ from dagster import (
     sensor,
 )
 from nged_data.ckan_client import NgedCkanClient, httpx_get_with_auth
+from nged_data.process_flows import process_live_primary_substation_flows
 
 # Define Partitions
 # We use Multi-Partitions so every day's download is saved uniquely by (Date, Name)
@@ -31,15 +33,15 @@ composite_def = MultiPartitionsDefinition(
 )
 
 
-class CkanConfig(Config):
+class CkanCsvConfig(Config):
     url: str
 
 
 @asset(partitions_def=composite_def)
-def live_primary_csv(context: AssetExecutionContext, config: CkanConfig) -> Path:
+def live_primary_csv(context: AssetExecutionContext, config: CkanCsvConfig) -> Path:
     # Retrieve the keys
     partition = composite_def.get_partition_key_from_str(context.partition_key).keys_by_dimension
-    date_str = partition["last_modified_date"]
+    last_modified_date_str = partition["last_modified_date"]
     substation_name = partition["substation"]
     csv_filename = PurePosixPath(config.url).name
 
@@ -52,12 +54,33 @@ def live_primary_csv(context: AssetExecutionContext, config: CkanConfig) -> Path
     # Save CSV file to disk
     # TODO(Jack): Don't save here? Instead, return the CSV file and let an IO manager save it??
     dst_full_path = Path(
-        Path("data") / "NGED" / "raw" / "live_primary_flows" / date_str / csv_filename
+        Path("data") / "NGED" / "raw" / "live_primary_flows" / last_modified_date_str / csv_filename
     )
     dst_full_path.parent.mkdir(exist_ok=True, parents=True)
     dst_full_path.write_bytes(response.content)
 
     return dst_full_path
+
+
+@asset(partitions_def=composite_def)
+def live_primary_parquet(context: AssetExecutionContext, live_primary_csv: Path) -> None:
+    df_of_new_data = process_live_primary_substation_flows(live_primary_csv)
+    parquet_path = (
+        Path("data")
+        / "NGED"
+        / "parquet"
+        / "live_primary_flows"
+        / live_primary_csv.with_suffix(".parquet").name
+    )
+    if parquet_path.exists():
+        df_of_old_data = pl.read_parquet(parquet_path)
+        merged_df = pl.concat((df_of_old_data, df_of_new_data), how="vertical")
+    else:
+        parquet_path.parent.mkdir(exist_ok=True, parents=True)
+        merged_df = df_of_new_data
+    merged_df = merged_df.unique(subset="timestamp")
+    merged_df = merged_df.sort(by="timestamp")
+    merged_df.write_parquet(parquet_path, compression="zstd")
 
 
 download_live_primary_csvs = define_asset_job(
@@ -100,7 +123,7 @@ def live_primaries_sensor(context: SensorEvaluationContext) -> SensorResult:
                 # runs for the same data on every tick.
                 run_key=f"{last_modified_date_str}|{substation_name}",
                 partition_key=partition_key,
-                run_config=RunConfig(ops={"live_primary_csv": CkanConfig(url=csv_url)}),
+                run_config=RunConfig(ops={"live_primary_csv": CkanCsvConfig(url=csv_url)}),
                 tags={
                     "nged/substation_name": substation_name,
                     "nged/substation_type": "primary",
