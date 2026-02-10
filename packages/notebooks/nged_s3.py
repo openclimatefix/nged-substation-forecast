15505ed75d64a19483abd430b9da88ca77e255c8
Jack Kelly <jack@openclimatefix.org>
2026-02-10 19:17:59 +0000

    Use consts

--- packages/notebooks/nged_s3.py
+++ packages/notebooks/nged_s3.py
@@ -7,15 +7,20 @@ with app.setup:
     from dotenv import load_dotenv
     import obstore
     import os
+    from typing import Final
 
     load_dotenv()
+
+    BUCKET_URL: Final[str] = os.environ["NGED_S3_BUCKET_URL"]
+    BUCKET: Final[str] = os.environ["NGED_S3_BUCKET"]
+    ACCESS_KEY: Final[str] = os.environ["NGED_S3_BUCKET_ACCESS_KEY"]
+    SECRET_KEY: Final[str] = os.environ["NGED_S3_BUCKET_SECRET"]
+    REGION: Final[str] = os.environ["NGED_S3_BUCKET_REGION"]
 
 
 @app.cell(disabled=True)
 def _():
-    http_store = obstore.store.HTTPStore(
-        url="https://nged-object-storage-dev-load-forecasting-data-eu-west-2.s3.eu-west-2.amazonaws.com/outbound/",
-    )
+    http_store = obstore.store.HTTPStore(url=BUCKET_URL)
     list(http_store.list())
     return
 
@@ -23,10 +28,10 @@ def _():
 @app.cell(disabled=True)
 def _():
     s3_store = obstore.store.S3Store(
-        bucket="nged-object-storage-dev-load-forecasting-data",
-        access_key_id=os.environ["NGED_S3_BUCKET_ACCESS_KEY"],
-        secret_access_key=os.environ["NGED_S3_BUCKET_SECRET"],
-        region=os.environ["NGED_S3_BUCKET_REGION"],
+        bucket=BUCKET,
+        access_key_id=ACCESS_KEY,
+        secret_access_key=SECRET_KEY,
+        region=REGION,
     )
     list(s3_store.list(prefix="outbound"))
     return
@@ -35,12 +40,11 @@ def _():
 @app.cell
 def _():
     s3_from_url = obstore.store.S3Store.from_url(
-        url="https://nged-object-storage-dev-load-forecasting-data-eu-west-2.s3.eu-west-2.amazonaws.com",
+        url=BUCKET_URL,
         config={
-            "aws_access_key_id": os.environ["NGED_S3_BUCKET_ACCESS_KEY"],
-            "aws_secret_access_key": os.environ["NGED_S3_BUCKET_SECRET"],
-            # Optional: Specify region if not inferred correctly from URL
-            "aws_region": os.environ["NGED_S3_BUCKET_REGION"],
+            "aws_access_key_id": ACCESS_KEY,
+            "aws_secret_access_key": SECRET_KEY,
+            "aws_region": REGION,
         },
     )
     s3_from_url
@@ -56,13 +60,12 @@ def _(s3_from_url):
 @app.cell
 def _():
     s3_from_bucket_and_config = obstore.store.S3Store(
-        "nged-object-storage-dev-load-forecasting-data",
+        BUCKET,
         config={
-            "aws_access_key_id": os.environ["NGED_S3_BUCKET_ACCESS_KEY"],
-            "aws_secret_access_key": os.environ["NGED_S3_BUCKET_SECRET"],
-            "aws_region": os.environ["NGED_S3_BUCKET_REGION"],
-            # Explicitly set the HTTPS endpoint
-            "aws_endpoint": "https://nged-object-storage-dev-load-forecasting-data-eu-west-2.s3.eu-west-2.amazonaws.com",
+            "aws_access_key_id": ACCESS_KEY,
+            "aws_secret_access_key": SECRET_KEY,
+            "aws_region": REGION,
+            "aws_endpoint": BUCKET_URL,  # Explicitly set the HTTPS endpoint
             "aws_allow_http": "false",  # Enforce HTTPS (default is usually false for http)
         },
     )
@@ -73,11 +76,6 @@ def _():
 @app.cell
 def _(s3_from_bucket_and_config):
     list(s3_from_bucket_and_config.list("outbound"))
-    return
-
-
-@app.cell
-def _():
     return
 
 
