8874d2e7e1fcb8a72fb6a633537b4bcb7127737d
Jack Kelly <jack@openclimatefix.org>
2026-02-16 21:18:18 +0000

    Almost finished computing the weights for the mean of the NWP grid boxes for each H3 hexagon

--- packages/dynamical_data/ecmwf_to_h3_delta_lake.py
+++ packages/dynamical_data/ecmwf_to_h3_delta_lake.py
@@ -7,6 +7,7 @@ with app.setup:
     import marimo as mo
     import polars as pl
     import h3.api.numpy_int as h3
+    import polars_h3 as plh3
     import shapely.geometry
     import shapely.wkt
     from typing import Final
@@ -24,6 +25,8 @@ def _():
 @app.cell
 def _():
     # TODO: Move all this geo code into a new `package/geo_utils` package.
+
+    # Downloaded from https://onsdigital.github.io/uk-topojson/ for the year 2025.
     with open("england_scotland_wales.geojson") as f:
         file_contents = f.read()
     return (file_contents,)
@@ -54,7 +57,7 @@ def _(shape):
 
     cells = h3.geo_to_cells(shape, res=H3_RES)
 
-    df = pl.DataFrame({"h3_index": list(cells)}, schema={"h3_index": pl.UInt64})
+    df = pl.DataFrame({"h3_index": list(cells)}, schema={"h3_index": pl.UInt64}).sort("h3_index")
     df
     return H3_RES, df
 
@@ -75,6 +78,56 @@ def _(df):
         opacity=0.2,
     )
     Map(layer)
+    return
+
+
+@app.cell
+def _(df):
+    df_with_children = df.with_columns(h3_res7=plh3.cell_to_children("h3_index", 7)).explode(
+        "h3_res7"
+    )
+    return (df_with_children,)
+
+
+@app.cell
+def _(df_with_children):
+    # --- 3. The "Coordinate Lookup" Step ---
+    # Instead of spatial join, we compute the NWP Key mathematically
+    GRID_SIZE = 0.25
+
+    # TODO: Need to think if ECMWF's grid boxes are centered on these coords, and how that interacts with our code below:
+
+    df_with_grid_x_y = df_with_children.with_columns(
+        # MATHEMATICAL MAPPING: Round down to nearest 0.25 degree
+        # This effectively bins every child into an NWP box
+        h3_res7_grid_cell=pl.struct(
+            y=(plh3.cell_to_lat("h3_res7") / GRID_SIZE).floor().cast(pl.Int64),
+            x=(plh3.cell_to_lng("h3_res7") / GRID_SIZE).floor().cast(pl.Int64),
+        ),
+    )
+
+    df_with_grid_x_y
+    return (df_with_grid_x_y,)
+
+
+@app.cell
+def _(df_with_grid_x_y):
+    df_with_counts = (
+        df_with_grid_x_y.group_by("h3_index")
+        .agg(grid_cell_counts=pl.col("h3_res7_grid_cell").value_counts())
+        .with_columns(
+            total=pl.col("grid_cell_counts").list.agg(pl.element().struct.field("count").sum())
+        )
+        .explode("grid_cell_counts")
+        .unnest("grid_cell_counts")
+        .with_columns(propotion=pl.col.count / pl.col.total)
+    )
+    df_with_counts
+
+    # TODO:
+    # 1. Join h3_res7_grid_cell with the actual NWP data, to end up with a dataframe that has `proportion` and the raw NWP value
+    # 2. Multiply `proportion` with each NWP value.
+    # 3. Groupby h3_index, and sum each NWP value column
     return
 
 
