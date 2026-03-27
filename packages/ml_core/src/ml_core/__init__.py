88b4795934e4e9e7505e99863589efc7d21d7d00
Jack Kelly <jack@openclimatefix.org>
2026-03-27 16:48:26 +0000

    Delete FeatureAsset

--- packages/ml_core/src/ml_core/__init__.py
+++ packages/ml_core/src/ml_core/__init__.py
@@ -1,4 +1,3 @@
-from .assets import FeatureAsset as FeatureAsset
 from .model import BaseForecaster as BaseForecaster
 from .utils import evaluate_and_save_model as evaluate_and_save_model
 from .utils import train_and_log_model as train_and_log_model
