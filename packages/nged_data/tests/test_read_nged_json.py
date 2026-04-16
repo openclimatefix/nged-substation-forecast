cf5fae8d70dbf4320cb910286a83b17acda3d267
Jack Kelly <jack@openclimatefix.org>
2026-04-16 16:36:33 +0100

    Tweaked example JSON files

--- packages/nged_data/tests/test_read_nged_json.py
+++ packages/nged_data/tests/test_read_nged_json.py
@@ -1,10 +1,11 @@
-from nged_data.read_nged_json import nged_json_to_metadata_df_and_time_series_df
 import os
+
+from nged_data.read_nged_json import nged_json_to_metadata_df_and_time_series_df
 
 
 def test_nged_json_to_metadata_df_and_time_series_df_file_10():
     data_dir = "packages/nged_data/tests/data"
-    file_path = os.path.join(data_dir, "TimeSeries_10_20160101T003000Z_20260326T083000Z.json")
+    file_path = os.path.join(data_dir, "TimeSeries_10.json")
 
     with open(file_path, "rb") as f:
         json_bytes = f.read()
@@ -21,7 +22,7 @@ def test_nged_json_to_metadata_df_and_time_series_df_file_10():
 
 def test_nged_json_to_metadata_df_and_time_series_df_file_11():
     data_dir = "packages/nged_data/tests/data"
-    file_path = os.path.join(data_dir, "TimeSeries_11_20160101T003000Z_20260326T083000Z.json")
+    file_path = os.path.join(data_dir, "TimeSeries_11.json")
 
     with open(file_path, "rb") as f:
         json_bytes = f.read()
