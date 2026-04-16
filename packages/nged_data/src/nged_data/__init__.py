8a3a992a5c96b004fde4ff901b97267d4d92630a
Jack Kelly <jack@openclimatefix.org>
2026-04-16 17:58:16 +0100

    Delete clean.py

--- packages/nged_data/src/nged_data/__init__.py
+++ packages/nged_data/src/nged_data/__init__.py
@@ -1,4 +1,3 @@
-from .clean import clean_power_time_series
 from .storage import append_to_delta, upsert_metadata
 
-__all__ = ["clean_power_time_series", "append_to_delta", "upsert_metadata"]
+__all__ = ["append_to_delta", "upsert_metadata"]
