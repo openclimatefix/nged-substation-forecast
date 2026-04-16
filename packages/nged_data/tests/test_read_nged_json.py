88aad843ad43cbcfe84efa1a76c11f88395a7ef7
Jack Kelly <jack@openclimatefix.org>
2026-04-16 17:39:50 +0100

    more rigorously check the test time series

--- packages/nged_data/tests/test_read_nged_json.py
+++ packages/nged_data/tests/test_read_nged_json.py
@@ -6,13 +6,20 @@ from nged_data.read_nged_json import nged_json_to_metadata_df_and_time_series_df
 
 
 @pytest.mark.parametrize(
-    "filename, expected_time_series_id", [("TimeSeries_10.json", 10), ("TimeSeries_11.json", 11)]
+    "filename, expected_time_series_id, expected_name, expected_units, expected_h3",
+    [
+        ("TimeSeries_10.json", 10, "SPILSBY 33 11kV S STN", "MW", 599423199024775167),
+        ("TimeSeries_11.json", 11, "INGOLDMELLS 33 11kV S STN", "MVA", 599422966022799359),
+    ],
 )
-def test_nged_json_to_metadata_df_and_time_series_df(filename: str, expected_time_series_id: int):
-    data_dir = Path(
-        "tests/data"
-    )  # FIXME: Use a path that works correctly no matter if you run pytest in the root uv workspace directory or in packages/nged_data
-    file_path = data_dir / filename
+def test_nged_json_to_metadata_df_and_time_series_df(
+    filename: str,
+    expected_time_series_id: int,
+    expected_name: str,
+    expected_units: str,
+    expected_h3: int,
+):
+    file_path = Path(__file__).parent / "data" / filename
 
     with open(file_path, "rb") as f:
         json_bytes = f.read()
@@ -25,3 +32,10 @@ def test_nged_json_to_metadata_df_and_time_series_df(filename: str, expected_tim
     assert not metadata_df.is_empty()
     assert not time_series_df.is_empty()
     assert metadata_df["time_series_id"].item() == expected_time_series_id
+    assert metadata_df["time_series_name"].item() == expected_name
+    assert metadata_df["units"].item() == expected_units
+    assert metadata_df["time_series_type"].item() == "Disaggregated Demand"
+    assert metadata_df["licence_area"].item() == "EMids"
+    assert metadata_df["substation_type"].item() == "Primary"
+    assert "POLYGON" in metadata_df["area_wkt"].item()
+    assert metadata_df["h3_res_5"].item() == expected_h3
