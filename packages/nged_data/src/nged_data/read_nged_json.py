2b7212fab4ad4c25312bfdc76a1efbdbfbafc2d7
Jack Kelly <jack@openclimatefix.org>
2026-04-17 10:46:33 +0100

    Add TODO. And fix ty error

--- packages/nged_data/src/nged_data/read_nged_json.py
+++ packages/nged_data/src/nged_data/read_nged_json.py
@@ -1,9 +1,16 @@
 import re
+from typing import Final
 
 import patito as pt
 import polars as pl
 import polars_h3
 from contracts.power_schemas import PowerTimeSeries, TimeSeriesMetadata
+
+# TODO: When we move to using multiple NWPs (at different resolutions), we should use a high
+# H3 resolution for TimeSeriesMetadata, say res 9, and then use `polars_h3.cell_to_parent` to
+# dynamically convert `TimeSeriesMetadata.h3_res_9` to `NwpMetaData.h3_resolution` before joining.
+# See https://github.com/openclimatefix/nged-substation-forecast/issues/114
+_H3_RESOLUTION: Final[int] = 5
 
 
 def nged_json_to_metadata_df_and_time_series_df(
@@ -40,9 +47,11 @@ def _extract_time_series_metadata(df: pl.DataFrame) -> pt.DataFrame[TimeSeriesMe
     metadata_df = metadata_df.unnest("Area", separator="_")
     metadata_df = metadata_df.rename({col: _camel_to_snake(col) for col in metadata_df.columns})
 
-    # Compute H3 index at resolution 5
+    # Compute H3 index
     metadata_df = metadata_df.with_columns(
-        h3_res_5=polars_h3.latlng_to_cell(pl.col("latitude"), pl.col("longitude"), 5)
+        polars_h3.latlng_to_cell(pl.col("latitude"), pl.col("longitude"), _H3_RESOLUTION).alias(
+            f"h3_res_{_H3_RESOLUTION}"
+        )
     )
 
     metadata_df = metadata_df.sort("time_series_id")
@@ -67,10 +76,10 @@ def _extract_power_time_series(
     )
 
     time_series_df = time_series_df.with_columns(time_series_id=pl.lit(time_series_id))
-
+    time_series_df = pt.DataFrame(time_series_df).set_model(PowerTimeSeries).drop().cast()
     time_series_df = PowerTimeSeries.sort(time_series_df)
 
-    return pt.DataFrame(time_series_df).set_model(PowerTimeSeries).drop().cast().validate()
+    return time_series_df.validate()
 
 
 def _camel_to_snake(camel_str: str) -> str:
